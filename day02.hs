3be0feaf16014f97b66ac1212898aba97c385dc7
Glen Bradley <glenbolake@gmail.com>
2022-12-02 14:56:26 -0500

    Day 2 part 2

--- day02.hs
+++ day02.hs
@@ -1,8 +1,42 @@
-data Throw = Rock | Paper | Scissors deriving (Enum, Show)
+data Throw = Rock | Paper | Scissors deriving (Enum, Show, Bounded)
 type Matchup = (Throw, Throw)
 
-parseInput :: String -> [Matchup]
-parseInput input = map parseLine (lines input)
+allThrows = [minBound..]::[Throw]
+
+-- Logic for scoring a game
+
+scoreThrow :: Throw -> Int
+scoreThrow Rock = 1
+scoreThrow Paper = 2
+scoreThrow Scissors = 3
+
+beats :: Throw -> Throw -> Bool
+Rock `beats` Scissors  = True
+Scissors `beats` Paper = True
+Paper `beats` Rock     = True
+_ `beats` _            = False
+
+scoreWinner :: Matchup -> Int
+scoreWinner (them, me)
+    | me `beats` them = 6
+    | them `beats` me = 0
+    | otherwise       = 3
+
+scoreMatchup :: Matchup -> Int
+scoreMatchup (them, me) = scoreThrow me + scoreWinner (them, me)
+
+-- Logic for deciding how to win/lose/draw
+
+deduceThrow :: Throw -> Char -> Throw
+deduceThrow them must
+    -- must win
+    | must == 'Z' = head [me | me <- allThrows, me `beats` them]
+    -- must lose
+    | must == 'X' = head [me | me <- allThrows, them `beats` me]
+    -- must dra
+    | otherwise   = them
+
+-- Input parsing
 
 asThrow :: Char -> Throw
 asThrow 'A' = Rock
@@ -12,27 +46,20 @@ asThrow 'X' = Rock
 asThrow 'Y' = Paper
 asThrow 'Z' = Scissors
 
-parseLine :: String -> Matchup
-parseLine line = (asThrow (head line), asThrow (last line))
+parseInput :: (String -> Matchup) -> String -> [Matchup]
+parseInput parser input = map parser (lines input)
 
-scoreThrow :: Throw -> Int
-scoreThrow Rock = 1
-scoreThrow Paper = 2
-scoreThrow Scissors = 3
+-- Part 1: Second column is a throw
+parseLineTwoThrows :: String -> Matchup
+parseLineTwoThrows line = (asThrow (head line), asThrow (last line))
 
-scoreWinner :: Matchup -> Int
-scoreWinner (Rock, Scissors) = 0
-scoreWinner (Scissors, Paper) = 0
-scoreWinner (Paper, Rock) = 0
-scoreWinner (Rock, Rock) = 3
-scoreWinner (Paper, Paper) = 3
-scoreWinner (Scissors, Scissors) = 3
-scoreWinner (Rock, Paper) = 6
-scoreWinner (Paper, Scissors) = 6
-scoreWinner (Scissors, Rock) = 6
+-- Part 2: Second column is whether to win
+parseLineStrategy :: String -> Matchup
+parseLineStrategy line =
+    let them = asThrow (head line)
+    in (them, deduceThrow them (last line))
 
-scoreMatchup :: Matchup -> Int
-scoreMatchup (a,b) = scoreThrow b + scoreWinner (a,b)
+solve strategy input = sum [scoreMatchup m | m <- parseInput strategy input]
 
-part1 input = sum [scoreMatchup m | m <- parseInput input]
-
+part1 input = solve parseLineTwoThrows input
+part2 input = solve parseLineStrategy input
