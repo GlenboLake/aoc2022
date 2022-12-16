732f08784213c0dea1e86eaf423b5db955ea62b7
Glen Bradley <glenbolake@gmail.com>
2022-12-16 13:56:08 -0500

    Day 15 part 1

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,11 +1,18 @@
 module Day15 where
 import AOC (solve)
-import Data.List (nub)
+import Data.List (nub, sort)
 import System.IO.Unsafe (unsafePerformIO)
 
 sample = unsafePerformIO . readFile $ "inputs/sample15.txt"
 
 type Coord = (Int, Int)
+data Range = Range { left::Int, right::Int } | Nil deriving (Show, Eq, Ord)
+
+sizeOf :: Range -> Int
+sizeOf Range { left=l, right=r } = r-l+1
+
+contains :: Range -> Int -> Bool
+contains (Range l r) i = i >= l && i <= r
 
 manhattan :: Coord -> Coord -> Int
 manhattan (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
@@ -21,18 +28,40 @@ parseInput input = map parseSensor $ lines input
   where parseSensor line = let (a:b:c:d:_) = map (read :: String -> Int) $ filter (/="") $ map (filter isNumeric) $ words line
                            in Sensor { loc = (a, b), beacon = (c, d) }
 
-checkSensor :: Sensor -> Int -> [Coord]
-checkSensor (Sensor { loc = l@(lx, ly), beacon = b@(bx, by) }) y = do
-  let m = manhattan l (bx,by)
-  let width = m - abs (y-ly)
-  let cs = map (,y) [lx-width..lx+width]
-  filter (/=b) cs
+checkSensor :: Sensor -> Int -> Range
+checkSensor (Sensor { loc = l@(lx, ly), beacon = b@(bx, by) }) y
+  | width <= 0 = Nil
+  | otherwise  = Range (lx-width) (lx+width)
+  where m = manhattan l (bx,by)
+        width = m - abs (y-ly)
 
-checkRow :: [Sensor] -> Int -> [Coord]
-checkRow [] _ = []
-checkRow (s:ss) y = nub $ checkSensor s y ++ checkRow ss y
+-- combine ranges together. This function assumes they
+-- are already sorted by the min bound and Nil has been removed
+overlaps :: Range -> Range -> Bool
+a `overlaps` b = (max (left a) (left b)) - (min (right a) (right b)) <= 1
 
-part1 input = length $ checkRow (parseInput input) 2_000_000
+combineTwoRanges :: Range -> Range -> Range
+combineTwoRanges a b = Range {
+  left = min (left a) (left b),
+  right = max (right a) (right b)
+}
+
+combine :: [Range] -> [Range]
+combine [] = []
+combine [a] = [a]
+combine [a,b] = [combineTwoRanges a b]
+combine (a@Range { left=la, right=ra }:b@Range { left=lb, right=rb }:rs)
+  | lb > ra + 1 = (a:combine (b:rs))
+  | otherwise   = combine (combineTwoRanges a b:combine rs)
+
+part1 input = do
+  let y = 2_000_000
+  let sensors = parseInput input
+  let beacons = nub $ map beacon sensors
+  let beaconsOnRow = filter (==y) $ map snd beacons
+  let ranges = combine $ sort $ filter (/= Nil) $ map (flip checkSensor y) $ sensors
+  let numBeaconsInRanges = length $ nub [ b | b <- beaconsOnRow, r <- ranges, r `contains` b ]
+  (sum $ map sizeOf ranges) - numBeaconsInRanges
 
 main input = do
   solve part1 input
